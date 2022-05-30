7d7ee25d5711282914e581582cfe76f0be7be00b
Adarsh Dhiman <adarshdhiman111@gmail.com>
2022-05-30 20:42:56 +0530

    first commit

---
+++ stockapp.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas_datareader as data
+import streamlit as st
+
+st.title('Stock Trend Prediction')
+
+start = '2010-01-01'
+end = '2019-12-31'
+
+user_input = st.text_input('Enter Stock Ticker', 'AAPL')
+df = data.DataReader(user_input, 'yahoo', start, end)
+
+st.subheader('Data from 2010-2019')
+st.write(df.describe())
+
+st.subheader('Closing Price vs Time chart')
+fig = plt.figure(figsize = (12,6))
+plt.plot(df.Close)
+st.pyplot(fig)
+
+st.subheader('Closing Price vs Time chart 100MA')
+ma100 = df.Close.rolling(100).mean()
+fig = plt.figure(figsize = (12,6))
+plt.plot(df.Close)
+plt.plot(ma100,'r')
+st.pyplot(fig)
+
+st.subheader('Closing Price vs Time chart with 100MA & 200MA')
+ma200 = df.Close.rolling(200).mean()
+fig = plt.figure(figsize = (12,6))
+plt.plot(df.Close)
+plt.plot(ma100,'r')
+plt.plot(ma200,'g')
+st.pyplot(fig)
